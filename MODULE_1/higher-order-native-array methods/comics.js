e3aa860d99328c4e5a224707264ab8529ca522c8
Gigi Scarborough <gigimscarborough@gmail.com>
2023-05-01 13:49:12 -0400

    native-array notes

---
+++ MODULE_1/higher-order-native-array methods/comics.js
@@ -0,0 +1,8 @@
+const comics = [
+    { title: "Calvin & Hobbes", author: "Bill Watterson", kind: "print" },
+    { title: "Zen Pencils", author: "Gavin Aung Than", kind: "web" },
+    { title: "Nancy", author: "Olivia James", kind: "print" },
+    { title: "False Knees", author: "Joshua Barkman", kind: "web" },
+  ];
+
+module.exports={comics}
