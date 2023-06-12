95a92fbe72c9dc6b2ebbaf6a9d04b4d93cef464a
mckiernantim <38140787+mckiernantim@users.noreply.github.com>
2023-06-12 17:38:20 -0400

    Update isPalindrome.test.js

--- algoirthms/src/__tests__/isPalindrome.test.js
+++ algoirthms/src/__tests__/isPalindrome.test.js
@@ -3,13 +3,11 @@ const { isPalindrome } = require('../isPalindrome/isPalindrome.js');
 test('returns true for a palindrome string', () => {
   expect(isPalindrome("racecar")).toBe(true);
   expect(isPalindrome("Madam")).toBe(true);
-  expect(isPalindrome("A man a plan a canal Panama")).toBe(true);
 });
 
 test('returns false for a non-palindrome string', () => {
   expect(isPalindrome("hello")).toBe(false);
   expect(isPalindrome("OpenAI")).toBe(false);
-  expect(isPalindrome("Coding is fun")).toBe(false);
 });
 
 
