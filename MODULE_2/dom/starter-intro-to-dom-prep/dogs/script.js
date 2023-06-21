e37314e1e52be6082ee97bb42868fa1136af2579
mckiernantim <mckiernantim@gmail.com>
2023-06-21 16:55:04 -0400

    adds pre-work to dom

---
+++ MODULE_2/dom/starter-intro-to-dom-prep/dogs/script.js
@@ -0,0 +1,68 @@
+// how do we know this script is connected to our HTML file?
+
+// can we write something here to make sure its working?
+
+// How do we add a <span> element to our h1?
+// The span element should come after the text.
+
+// how do we add text to this span?
+
+// using our CSS how do give it an underline?
+
+// how do we do this without leveraging our CSS file?
+// what are the following lines doing?
+const url = `https://placedog.net/500`;
+
+// what is this funciton doing?
+function _getRandomInt() {
+    return Math.floor(Math.random() * 500);
+}
+// what is this array representing?
+const dogInfo = [
+    {
+        name:"steve",
+        description:"hes a good boi",
+        imageNum:getRandomInt(),
+        featured:false
+    }
+    /*
+    
+        using the data above we will build functionality to add cards to our HTML
+    
+        These cards will have:
+            1: an <img> with our dogs;
+            2: an <h2> with our dogs name;
+            3: a <p> tag with some info on our dog
+    
+            The end result should look something like this:
+    
+        <article>
+            <img
+                class="animated"
+                src="https://placedog.net/500/450"
+                alt="Amber, the dog."
+            />
+            <h2>Amber</h2>
+            <p>A sweet little doggo that belongs to my friend.</p>
+        </article>
+    
+    */
+ 
+];
+
+// how do we create the tags above using JS?
+
+// how do we populate the attributes of our image?
+
+// how do we add text to our h2 and p tags?
+
+// how do we add a 'featured' class to our article tag?
+
+// lets add 6 dogs to our data - and do this six times
+
+// can we write  a helper function to handle this?
+
+
+
+
+
---
+++ MODULE_2/dom/starter-intro-to-dom-prep/nameplate/script.js
@@ -0,0 +1,26 @@
+// how do we know this is connected?
+
+// how can we be sure?
+
+// how can we add our own name to this?
+
+// how about our own birthday?
+
+// give your birthday some style wuthout using CSS
+
+// give your birthday some style using CSS
+
+
+// how can we select all the tags with an  'emoji'  class?
+
+// how ELSE can we do this?
+
+// what data type is this?
+
+// how can we iterate over it?
+
+
+// how can we remove ALL EMOJIS from this page?
+
+
+// 
