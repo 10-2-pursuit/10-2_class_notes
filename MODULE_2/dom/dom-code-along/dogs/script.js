c26b71faeed90961dfc7ddab1217efb71e1b5b1a
mckiernantim <mckiernantim@gmail.com>
2023-06-21 20:57:52 -0400

    adds dom notes

--- MODULE_2/dom/dom-code-along/dogs/script.js
+++ MODULE_2/dom/dom-code-along/dogs/script.js
@@ -1,15 +1,25 @@
 // how do we know this script is connected to our HTML file?
-
+// alert("hey Dwayne")
+console.log("script is connected")
 // can we write something here to make sure its working?
 
 // How do we add a <span> element to our h1?
+const h1 = document.querySelector("h1")
+let span = document.createElement("span")
+span.textContent = "  Hello 10.2!!!!!  ";
+console.log(h1)
 // The span element should come after the text.
 
 // how do we add text to this span?
+h1.append(span)
 
 // using our CSS how do give it an underline?
+span.classList.add("underline")
 
 // how do we do this without leveraging our CSS file?
+span.style.color = "blue";
+
+
 // what are the following lines doing?
 const url = `https://placedog.net/500`;
 
@@ -22,9 +32,41 @@ const dogInfo = [
     {
         name:"steve",
         description:"hes a good boi",
-        imageNum:getRandomInt(),
+        imageNum:_getRandomInt(),
         featured:false
+    },
+    {
+        name:"bryan",
+        description:"hes a good boi",
+        imageNum:_getRandomInt(),
+        featured:false
+    },
+    {
+        name:"bob",
+        description:"hes a good boi",
+        imageNum:_getRandomInt(),
+        featured:true
+    },
+    {
+        name:"catboy",
+        description:"hes a good boi",
+        imageNum:_getRandomInt(),
+        featured:false
+    },
+    {
+        name:"Billy",
+        description:"hes a good boi",
+        imageNum:_getRandomInt(),
+        featured:false
+    },
+    {
+        name:"benji",
+        description:"hes a good boi",
+        imageNum:_getRandomInt(),
+        featured:true
     }
+    
+];
     /*
     
         using the data above we will build functionality to add cards to our HTML
@@ -48,17 +90,54 @@ const dogInfo = [
     
     */
  
-];
 
 // how do we create the tags above using JS?
+const _createCard = (dog) => {
+    let article = document.createElement("article");
+    const { name, description, imageNum } = dog
+    
+    let image = _createImage(dog)
+    let h2 = document.createElement("h2");
+    let p = document.createElement("p");
+    // how do we add text to our h2 and p tags?
+    h2.textContent = name
+    p.textContent = description
+    // how do we populate the attributes of our image?
 
-// how do we populate the attributes of our image?
+    // how do we add a 'featured' class to our article tag?
+    article.append(h2, image, p)
+    let main = document.querySelector("main");
+    main.append(article)
+    
+}
 
-// how do we add text to our h2 and p tags?
+const _createImage = (dog) => {
+  const { name, description, imageNum } = dog;
+  let image = document.createElement("img");
+  image.src = url + "/" + imageNum;
+  image.alt = `${name}, ${description}`;
+  image.classList.add("animated");
+  return image;
+};
 
-// how do we add a 'featured' class to our article tag?
+for(let dog of dogInfo) {
+    _createCard(dog)
+}
+// get a handle on the item we want to remove
+const main = document.body.children[1]
+const dogToDelete = main.children[1]
+
+dogToDelete.remove()
+
+
+
+
 
 // lets add 6 dogs to our data - and do this six times
+
+
+
+
 
 // can we write  a helper function to handle this?
 
