b0f455ebb826efe519f8f95bf18abd0dc14f4594
mckiernantim <mckiernantim@gmail.com>
2023-06-25 10:50:32 -0400

    finishes events notes

--- MODULE_2/events/starter-events/script.js
+++ MODULE_2/events/starter-events/script.js
@@ -1,18 +1,38 @@
-const increment = document.querySelector(".increment");
+// 1 - get a handle on the node you want to create / update
 
-increment.addEventListener("click", (e) => {
-  const hearts = document.querySelector(".hearts");
-  hearts.textContent += "❤";
-});
+// 2 - update the proper key / values
 
-const decrement = document.querySelector(".decrement");
+// 3 - add it to the dom 
 
-decrement.addEventListener("click", () => {
-  const hearts = document.querySelector(".hearts");
-  hearts.textContent = hearts.textContent.slice(0, -1);
-});
+const incrementButtons = document.querySelectorAll(".increment");
+
+for (let button of incrementButtons) {
+  button.addEventListener("click", (e) => {
+    const hearts = e.target.parentNode.parentNode.querySelector(".hearts");
+    hearts.textContent += "❤";
+  });
+}
+
+const decrementButtons = document.querySelectorAll(".decrement");
+
+for (let button of decrementButtons) {
+  button.addEventListener("click", (e) => {
+    const hearts = e.target.parentNode.parentNode.querySelector(".hearts");
+    if(hearts.textContent.length > 3) {
+      hearts.textContent = hearts.textContent.slice(0, -1);
+    }
+  });
+}
 
 
 let card = document.querySelector('article');
 
 
+card.addEventListener("mouseenter", () => {
+  card.style.transform = "scale(1.1)";
+  
+})
+
+card.addEventListener("mouseleave", () => {
+  card.style.transform = "scale(1)"
+})
