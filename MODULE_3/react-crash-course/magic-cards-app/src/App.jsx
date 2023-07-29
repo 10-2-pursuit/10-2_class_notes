aad079ca6e2a1463a462185a75a067f764ea160e
mckiernantim <mckiernantim@gmail.com>
2023-07-29 15:20:30 -0400

    adds react-crash-course app

---
+++ MODULE_3/react-crash-course/magic-cards-app/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import { useState } from 'react'
+import CardList from './components/CardList'
+import MainDisplay from './components/MainDisplay'
+import useGetMTG   from "./hooks/getMtgCards"
+function App() {
+  // set an empty array for our state
+  // two items in array, 
+  // the STATE itself, and a funciton to update it
+  const [ allCards, setAllCards ] = useGetMTG([])
+  // this is how we establish state
+  // we return TWO items from the call
+  // the first item is the statefullVariable, the second is the function to update
+  const [ selectedCard, setSelectedCard ] = useState(null)
+  
+  return (
+    <div>
+      <h1 className='title'>Welcome To our MTG app</h1>
+    <div className='app-container'>
+      {/* this is passing PROPS */}
+     <MainDisplay selectedCard = { selectedCard }/>
+     <CardList allCards = { allCards } setSelectedCard = { setSelectedCard }/>
+    </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ MODULE_3/react-crash-course/magic-cards-app/src/components/MainDisplay.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Card from "./Card";
+// object oriented programming - everything is an object
+// { state: true, wheels: 4 }
+// class MainDisplay extends Component {
+//     render() {
+//         return (
+//             <div>
+
+//             </div>
+//         );
+//     }
+// }
+
+// functional programming - everything is a function
+// useState([]) => function call holds the logic
+// hasWheels(4) ;
+
+const MainDisplay = (props) => {
+  
+  const selected = props.selectedCard;
+
+  return selected ? (
+    <div className="main-display-container">
+        <h1>{ selected.name }</h1>
+      <Card card ={ selected } imageWidth = '350px'/>
+    </div>
+  ) : (
+    <div>
+      <h1>no card selected</h1>
+    </div>
+  );
+};
+
+export default MainDisplay;
