77cf8af04d5095a6897bd5d8f8dbd5ecf8673fe3
mckiernantim <mckiernantim@gmail.com>
2023-08-21 20:23:04 -0400

    break it

--- MODULE_3/git-and-teams/example.js
+++ MODULE_3/git-and-teams/example.js
@@ -1,4 +1,4 @@
-function simulateMergeConflict(oneOne, two) {
+function simulateMergeConflict(one, two) {
     
     const originalText = `This is the original text.`;
     const incomingChanges = `Incoming changes from another branch.`;
