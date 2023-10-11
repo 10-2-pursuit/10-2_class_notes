9dcb4405599f7c024488626f04fce716329e8b83
mckiernantim <mckiernantim@gmail.com>
2023-10-10 20:54:37 -0400

    adds SHOW, CREATE to bookmarks build

--- MODULE_4/express-mvc/bookmarks/app.js
+++ MODULE_4/express-mvc/bookmarks/app.js
@@ -3,13 +3,18 @@ const express = require("express");
 const bookmarks = require("./controllers/bookmarksController");
 // express() is turning it on
 // app - is the name of MY CAR
+// this is an instance of the express server 
 const app = express();
+
 // middleware - use another file in the middle of a req
-app.use("/bookmarks", bookmarks)
+// all it is - is a function that happens between req and res
+app.use(express.json());
+app.use("/bookmarks", bookmarks);
 
 const coolest = "Gen";
 // a callback is a function that is passed as
 // an arg or returned from function
+// http://localhost:8080/
 app.get("/", (req, res) => {
     console.log(coolest);
     const upperCoolest = coolest.toUpperCase();
--- MODULE_4/express-mvc/bookmarks/controllers/bookmarksController.js
+++ MODULE_4/express-mvc/bookmarks/controllers/bookmarksController.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 // this router is going to be in charge of all routes
 // for this ONE resource;  seperating our concerns;
 const bookmarks = express.Router();
@@ -8,12 +7,47 @@ const bookmarksData = require("../models/bookmark");
 
 // bookmarks
 // where is the route?  and what do we do?
+// what kind of request is this route handling?
+
+// GET
+// localhost:8080/bookmarks
+// what is the PARAMS, BODY ? optional, HEADERS READLINE 
+// HAS A METHOD it is either a GET request, or a POST request
+// or a PUT /PATCH request or a DELETE request;
+// if (req.readLine === "GET") then the .get() route handles  it
+
+// THIS IS THE INDEX ROUTE FOR http://localhost:8080/bookmarks
 bookmarks.get("/", (req, res) => {
     // what do we want to do
     console.log("sending all bookmarks data")
     // our HEADERS might only accept APPLICATION/JSON
     res.json(bookmarksData);
 })
+
+// bookmakrs/anything we can think of 
+// what does this route need to DO in order to send back
+// ONE specific item from our Data?
+// the ':' signals express that this is meant to be dynamic
+// and anything after the ":" is a variable OF that name
+bookmarks.get("/:index", (req, res) => {
+    const { index } = req.params;
+    console.log("SHOW ROUTE!@#$@!##@##!@#@!")
+    if (bookmarksData[index]) {
+        res.status(200).json(bookmarksData[index])
+    } else {
+        res.status(404).send("No bookmark at that index");
+    }
+})
+// localhost:8080/bookmarks 
+// Express knows that the request is POST so the POST request is fired;
+bookmarks.post("/", (req, res) => {
+    console.log("post route was hit!@#@!#!");
+    console.log(req.body, " <------ this is the req body sent from user")
+    bookmarksData.push(req.body);
+    res.status(200).json( {status: "OK", payload: bookmarksData[bookmarksData.length - 1]} )
+})
+
+bookmarks.delete("/", (req, res) => {})
 // this IS a callback function
 // 
 module.exports = bookmarks;
