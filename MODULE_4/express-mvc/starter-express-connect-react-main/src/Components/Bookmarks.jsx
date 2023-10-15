3f8d4087660c2dad85769934c657c68b874bf6cf
Gigi <gigi.scarborough@get-carrot.com>
2023-10-15 14:37:51 +0200

    add bookmarks frontend starter

---
+++ MODULE_4/express-mvc/starter-express-connect-react-main/src/Components/Bookmarks.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Bookmark from "./Bookmark";
+
+function Bookmarks() {
+  const [bookmarks] = useState([]);
+  return (
+    <div className="Bookmarks">
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Take me there</th>
+              <th>See this bookmark</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookmarks.map((bookmark, index) => {
+              return <Bookmark key={index} bookmark={bookmark} index={index} />;
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+}
+
+export default Bookmarks;
