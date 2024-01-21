b4216c657dd2288cd4102eb87d4da2ab2a7c36c3
Gigi <gigi.scarborough@get-carrot.com>
2024-01-21 17:06:46 -0500

    post lecture

--- MODULE_5/oop/oop-bonus/02-bind.js
+++ MODULE_5/oop/oop-bonus/02-bind.js
@@ -13,26 +13,26 @@
 
 // Let's take a look at example at bind in action:
 
- class Cat {
-  purr() {
-    console.log("meow");
-  }
+//  class Cat {
+//   purr() {
+//     console.log("meow");
+//   }
 
-  purrMore() {
-    this.purr();
-  }
-}
-let cat = new Cat();
+//   purrMore() {
+//     this.purr();
+//   }
+// }
+// let cat = new Cat();
 
-let sayMeow = cat.purrMore;
-sayMeow(); // TypeError: this.purr is not a function
+// let sayMeow = cat.purrMore;
+// sayMeow(); // TypeError: this.purr is not a function
 
 // You can now use the built in Function.bind to ensure your context, `this`,
 // is the cat object
-let boundCat = sayMeow.bind(cat);
+// let boundCat = sayMeow.bind(cat);
 
 // You still *need* to invoke the function
-boundCat(); // prints "meow"
+// boundCat(); // prints "meow"
 
 // That is the magic of bind! It allows you choose the context for your function. You don't need to restrict the context you'd like to bind to either - you can bind functions to any context.
 
@@ -67,19 +67,19 @@ boundCat(); // prints "meow"
 
 // Let's now revisit the above example of losing context in a callback and fix the context! Using the global.setTimeout function you want to call the cat.purrMore function with the context bound to the cat object.
 
-// class Cat {
-//     purr() {
-//       console.log("meow");
-//     }
+class Cat {
+    purr() {
+      console.log("meow");
+    }
   
-//     purrMore() {
-//       this.purr();
-//     }
-//   }
+    purrMore() {
+      this.purr();
+    }
+  }
   
-//   let cat = new Cat();
+  let cat = new Cat();
   
   // Bind the cat.purrMore function to the context of the cat object
-//   const boundPurr = cat.purrMore.bind(cat);
+  const boundPurr = cat.purrMore.bind(cat);
   
-//   global.setTimeout(boundPurr, 5000); // prints 5 seconds later: meow
+  global.setTimeout(boundPurr, 5000); // prints 5 seconds later: meow
