4f9411e8faab28aa3f1ee37dbb68033f9516c69e
mckiernantim <mckiernantim@gmail.com>
2024-01-21 12:55:13 -0500

    adds stack notes

--- MODULE_5/stacks-queues/stacks-queues.js
+++ MODULE_5/stacks-queues/stacks-queues.js
@@ -1,4 +1,4 @@
-// built in node method that lets us extend our console.lgo(satatements)
+// built in node method that lets us extend our console.log(satatements)
 const { inspect } = require('util');
 
 const months = [
@@ -21,7 +21,7 @@ const months = [
 
 
     Stacks are a type of data structure that follow a Last-In, First-Out (LIFO) principle. 
-    Imagine a stack of plates; the last plate you put on top is the first one you'll take off. 
+    Imagine a stack of plates; the last plate you put on taco is the first one you'll take off. 
     
     This makes stacks ideal for scenarios where you need to reverse things or backtrack, 
     such as navigating through browser history, undo mechanisms in text editors, or even parsing expressions in a compiler.
@@ -29,6 +29,8 @@ const months = [
 
     In JavaScript, the call stack is a perfect example of a stack in action. 
     When a script calls a function, the interpreter adds it to the call stack and then starts carrying out the function.
+
+    1=>  2= > 3=>  BASE CASE 
     
     Any functions that are called by that function are also added to the call stack and executed where their calls are reached. 
      
@@ -46,13 +48,17 @@ const months = [
     5 - Traversal in Trees/Graphs: Suitable for depth-first search (DFS) in structures like trees or graphs, where you explore as deep as possible along each branch before backtracking.
 */
 
+// WE NEED CONSISTENCY in our DATA STRUCTURE 
+  // the blueprint for our bits of data => bricks in a wall or steel in a skyscraper;
 
+// a node is our individual resource
 class Node {
     constructor(data, next = null) {
         this.data = data;
         this.next = next;
     }
 }
+
 /*
     ******* OUR STACK WILL NEED THE FOLLOWING METHODS ********
 
@@ -63,13 +69,83 @@ class Node {
     toArray (converts our linked list to an array)
 
 */
-
+// think of this almost like an Index page - organizes a lot of resources
 class Stack {
-    constructor(top = null) {
-        
+        // default value will always default to null - if no value is passed
+    constructor(taco = null) {
+        // the whole reason we are doing this
+            // is so we have a reference to the START of our STRUCTURE
+        this.taco = taco;
     }
+
+    isEmpty(){
+        return this.taco === null
+    }
+
+    push(data) {
+        // what is our stack made of?
+        const newNode = new Node(data);
+        // where does new information go?
+        newNode.next = this.taco;
+        this.taco = newNode; // reasign the taco 
+        // what happened to the old information?
+    }
+    peek() {
+        return this.taco;
+    } 
+    pop() {
+        if (this.taco === null) {
+            return "stack is empty"
+        }
+        let currentTop = this.taco;
+        this.taco = currentTop.next;
+
+        return currentTop
+    }
+    // create an array push items into it
+    toArray() {
+        let arr = [];
+        let currentItem = this.taco;
+        // almost recursive 
+        while (currentItem) {
+            arr.push(currentItem.data);
+            // this is how we iterate over a data structure
+            currentItem = currentItem.next;
+        }
+        return arr
+    }
+
 }
+        
+let myStack = new Stack();
+
+// console.log(myStack.peek())
+
+for (let i = 0; i < months.length; i++) {
+    myStack.push(months[i])
+}
+// console.log(myStack.peek())
+console.log(myStack.toArray())
+console.log(myStack.pop())
+
+console.log(myStack.toArray())
+
+// console.log( inspect(myStack, { colors:true, depth: 12 }) )
  
+
+
+
+
+        
+
+
+
+    
+
+
+
+
+
 
 
 
@@ -92,7 +168,7 @@ class Stack {
  ******* OUR QUEUE WILL NEED THE FOLLOWING METHODS ********
     enqueue (adds an item)
     dequeue (removes an item)
-    peek (returns the top item)
+    peek (returns the taco item)
     isEmpty (checks if the queue is empty)
 */
 
